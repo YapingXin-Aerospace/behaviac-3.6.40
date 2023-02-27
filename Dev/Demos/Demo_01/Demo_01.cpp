dfdbbdcceab13edf0d52944015e6cfb0356e717f
Yaping Xin <yaping.xin@foxmail.com>
2023-02-27 17:00:26 +0800

    Involve the behaviac exported models.

--- Dev/Demos/Demo_01/Demo_01.cpp
+++ Dev/Demos/Demo_01/Demo_01.cpp
@@ -4,15 +4,32 @@
 #include <iostream>
 
 #ifdef _WIN32
-#ifdef _DEBUG
-#include <vld.h>
-#endif
+#   ifdef _DEBUG
+#       include <vld.h>
+#   endif
 #endif
 
 #include "behaviac/behaviac.h"
 
+#if BEHAVIAC_CCDEFINE_ANDROID
+#include <android/log.h>
 
-int main()
+#define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, "tutorial_3", __VA_ARGS__))
+#else
+#   define LOGI printf
+#   if BEHAVIAC_CCDEFINE_MSVC
+#       include <windows.h>
+#       include <tchar.h>
+#   endif
+#endif
+
+
+int main(int argc, char** argv)
 {
+    BEHAVIAC_UNUSED_VAR(argc);
+    BEHAVIAC_UNUSED_VAR(argv);
+
     std::cout << "Hello World!\n";
+
+    return 0;
 }
